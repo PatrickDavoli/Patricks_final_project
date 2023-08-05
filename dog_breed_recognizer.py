d606c8a1939c71e3327e3e2f4c6e00775f79d229
Patrick Davoli <patrickdavoli01@gmail.com>
2023-08-05 00:21:41 -0400

    initial commit

--- dog_breed_recognizer.py
+++ dog_breed_recognizer.py
@@ -1,29 +1,33 @@
-#Code does everything besides loading the new image with the overlayed text
-
 import jetson_inference
 import jetson_utils
-from jetson_utils import cudaFont
-
 import argparse
+from jetson_utils import videoOutput, cudaFont, adaptFontSize
+
 
 parser = argparse.ArgumentParser()
 filename = input("what is the name of the image you'd like to load? ")
 parser.add_argument("--network", type=str, default="googlenet")
+parser.add_argument("output", type=str, default="", nargs='?')
 args = parser.parse_args()
-font = cudaFont()
+
 
 img = jetson_utils.loadImage(filename)
+output = videoOutput(args.output)
 net = jetson_inference.imageNet(args.network)
-dog_breed_idx = net.Classify(img)
+font = cudaFont(size=adaptFontSize(img.width))
+dog_breed_idx, confidence = net.Classify(img)
+
 
 if (dog_breed_idx in range(152, 278)):
     dog_breed = net.GetClassDesc(dog_breed_idx)
-    dog_breed_label = net.GetClassLabel(dog_breed)
-    print(f"imagenet: breed #{dog_breed} ({dog_breed_label})")
-    font.OverlayText(img, text=f"{dog_breed_label}",
-                 x=5, y=5 * (font.GetSize() + 5),
+    font.OverlayText(img, text=f"{dog_breed}",  
+                 width = 5, height = 5,
+                 x=5, y=0,
                  color=font.Black, background=font.White)
 else:
-    font.OverlayText(img, text=f"This wasn't recognized as an image of a dog, or the species isn't in the dataset!",
-                 x=5, y=5 * (font.GetSize() + 5),
+    font.OverlayText(img, text=f"This wasn't recognized as a dog!",
+                 width = 5, height = 5,
+                 x=5, y=0,
                  color=font.Black, background=font.White)
+
+output.Render(img)
